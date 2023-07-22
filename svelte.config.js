0d13fa26cd389ca583c911246b88359acb6d5e40
Prajjwal-08 <93479098+Prajjwal-08@users.noreply.github.com>
2023-07-22 14:30:52 +0530

    Add files via upload

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,9 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+            fallback: 'app.html' // may differ from host to host
+        })
 	},
 	preprocess: vitePreprocess()
 };
